d435065732a164de3f216111b8857691d3b39a6e
VGdC15 <LIbertad2801>
2025-04-27 20:42:48 -0300

    definicion de rutas y cards  de juegos en home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,23 @@ import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { QuienSoyComponent } from './components/quien-soy/quien-soy.component';
 import { RegistroComponent } from './components/registro/registro.component';
+import { AhorcadoComponent } from './pages/ahorcado/ahorcado.component';
+import { MayorMenorComponent } from './pages/mayor-menor/mayor-menor.component';
+import { PreguntadosComponent } from './pages/preguntados/preguntados.component';
+import { JuegoPropioComponent } from './pages/juego-propio/juego-propio.component';
 
 export const routes: Routes = [
         {"path": "home", component : HomeComponent},
         {"path": "login", component : LoginComponent},  
         {"path": "registro", component : RegistroComponent},
         {"path": "quien-soy", component : QuienSoyComponent},
+        {"path": "preguntados", component: PreguntadosComponent},
+        {"path": "ahorcado", component : AhorcadoComponent},
+        {"path": "mayor-menor", component: MayorMenorComponent},
+        {"path": "juego-propio", component: JuegoPropioComponent},
+        
+        
         {"path": '', redirectTo: 'home', pathMatch: 'full' },
         {"path": '**', redirectTo: '/home' }
+        
 ];
---
+++ src/app/pages/juego-propio/juego-propio.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-juego-propio',
+  imports: [],
+  templateUrl: './juego-propio.component.html',
+  styleUrl: './juego-propio.component.css'
+})
+export class JuegoPropioComponent {
+
+}
