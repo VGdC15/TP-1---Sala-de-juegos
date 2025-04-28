83a6dce5ff442b00dfefc7924bde8ba1eed19f4e
VGdC15 <LIbertad2801>
2025-04-27 21:48:18 -0300

    juego ahorcado en proceso

--- src/app/pages/ahorcado/ahorcado.component.ts
+++ src/app/pages/ahorcado/ahorcado.component.ts
@@ -1,11 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-ahorcado',
-  imports: [],
+  standalone: true,
   templateUrl: './ahorcado.component.html',
   styleUrl: './ahorcado.component.css'
 })
 export class AhorcadoComponent {
+  abecedario = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+  
+  palabraSecreta = signal('ANGULAR'); // Después HACERLO RANDOMMMM
+  letrasAdivinadas = signal<Set<string>>(new Set());
+  letrasErradas = signal<Set<string>>(new Set());
+  errores = signal(0);
+  estadoJuego = signal<'jugando' | 'ganado' | 'perdido'>('jugando');
 
+  seleccionarLetra(letra: string) {
+    if (this.estadoJuego() !== 'jugando') return;
+
+    if (this.palabraSecreta().includes(letra)) {
+      this.letrasAdivinadas().add(letra);
+      this.letrasAdivinadas.set(new Set(this.letrasAdivinadas()));
+      this.verificarGanador();
+    } else {
+      this.letrasErradas().add(letra);
+      this.letrasErradas.set(new Set(this.letrasErradas()));
+      this.errores.update(e => e + 1);
+      this.verificarPerdedor();
+    }
+  }
+
+  verificarGanador() {
+    const todasLetras = new Set(this.palabraSecreta().split(''));
+    const aciertos = this.letrasAdivinadas();
+    if ([...todasLetras].every(letra => aciertos.has(letra))) {
+      this.estadoJuego.set('ganado');
+      this.guardarResultado();
+    }
+  }
+
+  verificarPerdedor() {
+    if (this.errores() >= 6) { 
+      this.estadoJuego.set('perdido');
+      this.guardarResultado();
+    }
+  }
+
+  guardarResultado() {
+    console.log('Guardando resultado...');
+  }
 }
+
+
+
