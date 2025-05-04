e73134ff178a195fa224f2673328384b5aeb1ef1
VGdC15 <veronicac153@gmail.com>
2025-05-04 20:56:47 -0300

    mayor y menor: -estilos en css -correccion en logica de puntos -swal para finalizar el juego

--- src/app/pages/mayor-menor/mayor-menor.component.ts
+++ src/app/pages/mayor-menor/mayor-menor.component.ts
@@ -24,16 +24,15 @@ export class MayorMenorComponent implements OnInit {
       title: '¿Cómo se juega?',
       html: `
         <p style="text-align:center; color:#f8f8f2">
-        - Se mostrará una carta al azar.<br>
-        - Debés adivinar si la próxima carta será Mayor, Menor o Igual en valor numérico.<br>
-        - Si acertás:<br>
-          Ganás 1 punto por elegir Mayor o Menor correctamente.<br>
-          Ganás 2 puntos si acertás que es Igual.<br>
+          Se mostrará una carta al azar.<br>
+           Debés adivinar si la próxima carta será Mayor, Menor o Igual.<br>
+           Si acertás:<br>
+        - Ganás 100 puntos si elegís correctamente Mayor o Menor.<br>
+        - Ganás 500 puntos si acertás que es Igual.<br>
+        - Si fallás: perdés una vida.<br>
         - El valor de las cartas es:<br>
-          A = 1<br>
-          2 al 10 = su valor<br>
-          J = 11, Q = 12, K = 13<br>
-        - ¡Buena suerte!<br>
+        A = 1 | J = 11 | Q = 12 | K = 13<br>
+        ¡Buena suerte!<br>
         </p>
       `,
       icon: 'info',
@@ -65,11 +64,11 @@ export class MayorMenorComponent implements OnInit {
       const carta = res.cards[0];
       const nuevoValor = this.valorNumerico(carta.value);
       this.imagen.set(carta.image);
-
+  
       if (igual) {
         if (nuevoValor === this.previousValue) {
-          this.resultado.set('¡Correcto! ¡Igual!');
-          this.puntaje.update(p => p * 2);
+          this.resultado.set('¡Correcto, Igual!');
+          this.puntaje.update(p => p + 500);
         } else {
           this.resultado.set('Incorrecto :(');
           this.vidas.update(v => v - 1);
@@ -77,16 +76,32 @@ export class MayorMenorComponent implements OnInit {
       } else {
         if ((mayor && nuevoValor > this.previousValue) || (!mayor && nuevoValor < this.previousValue)) {
           this.resultado.set('¡Correcto!');
-          this.puntaje.update(p => p + 1);
+          this.puntaje.update(p => p + 100);
         } else {
           this.resultado.set('Incorrecto :(');
           this.vidas.update(v => v - 1);
         }
       }
-
+  
       this.previousValue = nuevoValor;
+      if (this.vidas() <= 0) {
+        Swal.fire({
+          title: '¡Perdiste!',
+          html: `<p style="text-align:center; color:#f8f8f2">Tu puntaje final fue: <strong>${this.puntaje()}</strong></p>`,
+          confirmButtonText: 'Volver a jugar',
+          background: '#1e1e2f',
+          color: '#f8f8f2',
+          confirmButtonColor: 'rgb(200, 27, 253)',
+          icon: 'error',
+          iconColor: 'orange',
+          width: '420px'
+        }).then(() => {
+          this.reiniciarJuego();
+        });
+      }
     });
   }
+  
   
 
   valorNumerico(valor: string): number {
