333d959ae91ea1317e139dd3bcdef481fe9137c3
VGdC15 <veronicac153@gmail.com>
2025-05-04 21:31:38 -0300

    ruta y boton de acceso al chat

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -38,36 +38,17 @@ export const routes: Routes = [
             ),
           canActivate: [authGuard],
         },
+
+        {
+          path: "chat",
+          loadComponent: () =>
+            import("./components/chat/chat.component").then(
+              (modulo) => modulo.ChatComponent
+            ),
+          canActivate: [authGuard],
+        },
       
         { path: "", redirectTo: "home", pathMatch: "full" },
         { path: "**", redirectTo: "/home" },
       ];
  
-// export const routes: Routes = [
-//         {"path": "home", component: HomeComponent},
-//         {"path": "juegos", 
-//                 canActivateChild:[authGuard],
-//                 //lazy loading
-//                 loadComponent: () => import("./components/juegos/juegos.component").then((modulo) => modulo.JuegosComponent),       
-//                 children:[
-//                         {"path": '', redirectTo: 'home', pathMatch: 'full' },
-//                         {"path": "preguntados", component: PreguntadosComponent},
-//                         {"path": "ahorcado", component : AhorcadoComponent},
-//                         {"path": "mayor-menor", component: MayorMenorComponent},
-//                         {"path": "juego-propio", component: JuegoPropioComponent},
-//                 ]
-//         },
-//         {"path": "login", component : LoginComponent},  
-//         {"path": "registro", component : RegistroComponent},
-
-//         {"path": "quien-soy", 
-//                 //lazy loading
-//                 loadComponent: () => import("./components/quien-soy/quien-soy.component").then((modulo) => modulo.QuienSoyComponent),
-//                 canActivate:[authGuard],
-//         },
-        
-        
-//         {"path": '', redirectTo: 'home', pathMatch: 'full' },
-//         {"path": '**', redirectTo: '/home' }
-        
-// ];
