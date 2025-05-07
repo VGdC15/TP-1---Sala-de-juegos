6b874a4f58ea7a669a36d9d780587bc179fc0286
VGdC15 <veronicac153@gmail.com>
2025-05-07 20:45:10 -0300

    maquetado de sala de puntajes

--- src/app/components/resultados/resultados.component.ts
+++ src/app/components/resultados/resultados.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-resultados',
-  imports: [],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './resultados.component.html',
   styleUrl: './resultados.component.css'
 })
