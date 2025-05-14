f9fe4b5e2d2b94c1ad1e9dce0b2f6a342db3ee77
VGdC15 <veronicac153@gmail.com>
2025-05-14 17:04:41 -0300

    limpieza de swal de confirmacion de guardado de puntos en db

--- src/app/pages/ahorcado/ahorcado.component.ts
+++ src/app/pages/ahorcado/ahorcado.component.ts
@@ -171,17 +171,6 @@ export class AhorcadoComponent {
   
     this.supabase.guardarPuntaje('puntajeAhorcado', puntaje, email, tiempo);
     this.puntajeGuardado = true;
-
-    Swal.fire({
-      title: '¡Puntaje guardado!',
-      icon: 'success',
-      background: '#1e1e2f',
-      color: '#f8f8f2',
-      confirmButtonColor: 'rgb(27, 253, 130)',
-      iconColor: 'limegreen',
-      timer: 1500,
-      showConfirmButton: false
-    });
   }
   
   elegirPalabraRandom() {
--- src/app/pages/preguntados/preguntados.component.ts
+++ src/app/pages/preguntados/preguntados.component.ts
@@ -262,16 +262,6 @@ export class PreguntadosComponent{
       this.supabase.guardarPuntaje('puntajePreguntados', puntaje, email, tiempo);
       this.puntajeGuardado = true;
   
-      Swal.fire({
-        title: '¡Puntaje guardado!',
-        icon: 'success',
-        background: '#1e1e2f',
-        color: '#f8f8f2',
-        confirmButtonColor: 'rgb(27, 253, 130)',
-        iconColor: 'limegreen',
-        timer: 1500,
-        showConfirmButton: false
-      });
     }
   
 }
